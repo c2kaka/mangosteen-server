5fd3c6b1a10cf5d5327cddc50e3b5d4558c59cdc
c2kaka <c2kaka@126.com>
2022-08-04 15:10:18 +0800

    feat: user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   post "/users", to: "users#create"
-  get "/users:id", to: "users#show"
+  get "/users/:id", to: "users#show"
 end
