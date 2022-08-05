392a6fd9150b30a360ce44468e815f4e15848df1
c2kaka <c2kaka@126.com>
2022-08-05 10:51:44 +0800

    feat: use kaminari page

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
-    def index
+  def index
+    items = Item.page params[:page]
+    render json: { resources: items }
+  end
 
-    end
-    def create
-
-    end
+  def create
+  end
 end
