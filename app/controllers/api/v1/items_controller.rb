021948e631b854aab8292f202f6b201505d151b1
c2kaka <c2kaka@126.com>
2022-08-05 11:11:29 +0800

    feat: add page in items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page params[:page]
-    render json: { resources: items }
+    items = Item.page(params[:page]).per(params[:per_page])
+    render json: { resources: items, pager: {
+        page: params[:page],
+        per_page: params[:per_page],
+        count: Item.count
+    }}
   end
 
   def create
